52ba096fb92b131ef6c0005aa5ee44653f6513e6
Giovanna Thron <giovanna.thron@gmail.com>
2014-09-29 16:27:19 -0700

    update starter code

--- code/TreeNode.py
+++ code/TreeNode.py
@@ -6,15 +6,19 @@ class TreeNode(object):
     '''
     def __init__(self):
         self.column = None  # (int)    index of feature to split on
+        self.split_value = None  # value of the feature to split on
+        self.categorical = True  # (bool) whether or not node is split on
+                                 # categorial feature
         self.name = None    # (string) name of feature (or name of class in the
                             #          case of a list)
-        self.children = {}  # (dict)   value of feature as key and child node
-                            #          as key
+        self.left = None    # (TreeNode) left child
+        self.right = None   # (TreeNode) right child
         self.leaf = False   # (bool)   true if node is a leaf, false otherwise
         self.classes = Counter()  # (Counter) only necessary for leaf node:
                                   #           key is class name and value is
                                   #           count of the count of data points
                                   #           that terminate at this leaf
+
 
     def as_string(self, level=0, prefix=""):
         '''
@@ -29,6 +33,17 @@ class TreeNode(object):
             result += indent + prefix + "\n"
         indent = "  |   " * level
         result += indent + "  " + str(self.name) + "\n"
-        for key, node in self.children.iteritems():
-            result += node.as_string(level + 1, str(key) + ":")
+        if not self.leaf:
+            if self.categorical:
+                left_key = str(self.value)
+                right_key = "no " + str(self.value)
+            else:
+                left_key = "< " + str(self.value)
+                right_key = ">= " + str(self.value)
+            result += self.left.as_string(level + 1, left_key + ":")
+            result += self.right.as_string(level + 1, right_key + ":")
         return result
+
+
+    def __str__(self):
+        return self.as_string()
--- code/recursion_practice.py
+++ code/recursion_practice.py
@@ -1,2 +1,42 @@
 from node import TreeNode
 
+
+def sum_digits(num):
+    '''
+    INPUT: int
+    OUTPUT: int
+
+    Return the sum of the digits of the integer num.
+    '''
+    pass
+
+
+def sum_tree(root):
+    '''
+    INPUT: TreeNode
+    OUTPUT: int
+
+    Return the sum of all the values in the binary tree with the given root.
+    '''
+    pass
+
+
+def print_all(root):
+    '''
+    INPUT: TreeNode
+    OUTPUT: None
+
+    Print all the values in the tree rooted at the given root (in any order).
+    '''
+    pass
+
+
+def build_coinflip_tree(k, value=""):
+    '''
+    INPUT: int, string
+    OUTPUT: TreeNode
+
+    Return the root of a binary tree representing all the possible outcomes
+    form flipping a coin k times. The value should be the value of the root.
+    '''
+    pass
